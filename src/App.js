76a7a305226496210984b7dc0db9f47066b052fe
mmtakeuchi <mikemtaki@gmail.com>
2021-10-25 16:41:31 -0700

    style header with background image and background color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 import Attribution from "./components/Attribution/Attribution";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Job Listing
       <Attribution />
     </div>
