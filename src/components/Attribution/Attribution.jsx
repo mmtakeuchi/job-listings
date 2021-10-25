3629a782cc96e7a3d05666f57e761ecd15f1b2f5
mmtakeuchi <mikemtaki@gmail.com>
2021-10-25 16:30:19 -0700

    created and added frontend mentod attribution

---
+++ src/components/Attribution/Attribution.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Attribution.scss";
+
+const Attribution = () => {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/mmtakeuchi/job-listings">Michael Takeuchi</a>.
+    </div>
+  );
+};
+
+export default Attribution;
